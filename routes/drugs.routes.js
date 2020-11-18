2c3a47f661364ecd356329b77aa76dc8a73ad09f
Alex Kan <teof95@gmail.com>
2020-11-18 11:15:16 +0100

    added webshop page

--- routes/drugs.routes.js
+++ routes/drugs.routes.js
@@ -8,18 +8,31 @@ const Drug = require('../models/drug.model')
 router.get("/drugs", (req, res, next) => {
     Drug.find({})
         .then((drugsDB) =>
-            res.render("drugs", { drugsDB }),
-        )
-        .catch((error) => `Error while fetching all drugs: ${error}`);
+            res.render("drugs", { drugsDB, user: req.session.user }))
+
+
+    .catch((error) => `Error while fetching all drugs: ${error}`);
 });
 
-//render the drugs details page.
+//render the drugs-details page.
 router.get("/Drugs/:id", (req, res, next) => {
     const { id } = req.params;
 
     Drug.findById(id)
-        .then((foundDrug) => res.render("drug-details", foundDrug))
+        .then((foundDrug) =>
+            res.render("drug-details", { foundDrug, user: req.session.user }))
         .catch((error) => `Error while finding book: ${error}`);
+});
+
+
+//render the webshop page
+router.get("/webshop", (req, res, next) => {
+    Drug.find({})
+        .then((drugsDB) =>
+            res.render("webshop", { drugsDB, user: req.session.user }))
+
+
+    .catch((error) => `Error while fetching all drugs: ${error}`);
 });
 
 
