5b24778746933ed23eebbb80ed1c1e6ff4326c5a
Alex Kan <teof95@gmail.com>
2020-11-18 15:20:37 +0100

    more styling added

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Drug = require('../models/drug.model')
+
 
 
 /* GET home page */
-router.get('/', (req, res, next) =>
-    res.render('index'));
+router.get('/', (req, res, next) => {
+    Drug.find({})
+        .then((drugsDB) =>
+            res.render("index", { drugsDB, user: req.session.user }))
+});
 
 module.exports = router;
