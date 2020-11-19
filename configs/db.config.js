130391d1cebb0ab39dd4e3e897bd8af7f5ed11d9
Alex Kan <teof95@gmail.com>
2020-11-19 15:32:34 +0100

    trying to connect to atlas

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(process.env.MONGOURIATLAS || 'mongodb://localhost/drugsdb', {
+    .connect(process.env.MONGOURIATLAS, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
     .catch(err => console.error('Error connecting to mongo', err));
+
+
+
+// process.env.MONGOURIATLAS ||
+// ||'mongodb://localhost/drugsdb'
